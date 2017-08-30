62869f94873ad935a9c222a31a3cdf6b4d32e064
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-30 10:06:54 -0700

    sets up rendering for list of artists

--- src/index.js
+++ src/index.js
@@ -7,38 +7,48 @@ class Home extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      artists: []
+    };
   }
 
-  // Lifecycle Methods
-
-  // Before component initalizes, go out and get data from Api
   componentWillMount() {
-    this.search();
+    // this.search();
+    fetch('/api/collection/artists/')
+      .then(res => res.json())
+      .then(artists => this.setState({ artists }));
   }
 
   render() {
-    var artists = _.map(this.state.artists, (artist) => {
-      return <li key={ artist.id }>{ artist.name }</li>;
-    })
+    // var artists = _.map(this.state.artists, (artist) => {
+    //   return <li key={ artist.id }>{ artist.name }</li>;
+    // })
+    // return (
+    //   <div>
+    //   <h1>Artist Search</h1>
+    //     <input ref="query" onChange={ (e) => { this.search(this.refs.query.value); } } type="text" />
+    //     <ul>{ artists }</ul>
+    //   </div>
+    // );
     return (
-      <div>
-      <h1>Artist Search</h1>
-        <input ref="query" onChange={ (e) => { this.search(this.refs.query.value); } } type="text" />
-        <ul>{ artists }</ul>
+      <div className="App">
+        <h1>Artists</h1>
+        {this.state.artists.map(artist =>
+          <div key={artist.slug}>{artist.name.display}</div>
+        )}
       </div>
     );
   }
 
-  search(artist = "Arthur_Dove") {
-    var url = `/api/collection/artists/${artist}`;
-    Request.get(url).then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
+  // search(artist = "Arthur_Dove") {
+  //   var url = `/api/collection/artists/${artist}`;
+  //   Request.get(url).then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // }
 }
 
 ReactDOM.render(
