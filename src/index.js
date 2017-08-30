5bc9acd23c7c73f02ad08014c8d26b9edc5aa490
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-30 14:30:30 -0700

    queries artworks and sets up display

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      artists: []
+      artworks: []
     };
   }
 
@@ -20,24 +20,36 @@ class Home extends React.Component {
   }
 
   render() {
-    var artists = _.map(this.state.artists, (artist) => {
-      return <li key={ artist.slug }><a href={ artist.web_url }>{ artist.name.display }</a></li>;
+    var artworks = _.map(this.state.artworks, (artwork) => {
+      var imgUrl = artwork.images[0] ? artwork.images[0].public_image : 'placeholder.jpg'
+      return <div className="col-md-3" key={ artwork.slug }>
+                <div className="card">
+                  <div className="card-block">
+                    <img className="card-img-top img-fluid" src={ imgUrl } alt="" />
+                    <h4 className="card-title">{ artwork.artists[0].artist.name_display }</h4>
+                    <p className="card-text">Collection { artwork.collection }</p>
+                    <p className="card-text">Type { artwork.type }</p>
+                  </div>
+                </div>
+              </div>;
     })
 
     return (
-      <div className="App">
-        <h1>artists</h1>
-        <input ref="query" onChange={ (e) => { this.search(this.refs.query.value); } } type="text" />
-        <ul>{ artists }</ul>
+      <div className="row justify-content-md-center">
+        <div className="col-md-12 text-center">
+          <h1>artworks</h1>
+          <input ref="query" onChange={ (e) => { this.search(this.refs.query.value); } } type="text" />
+            <div className="card-columns">{ artworks }</div>
+        </div>
       </div>
     );
   }
 
   search() {
-    fetch(`/api/collection/artists/?page=2`)
+    fetch(`/api/collection/artworks/`)
       .then(res => res.json())
       .catch(e => e)
-      .then(artists => this.setState({ artists }));
+      .then(artworks => this.setState({ artworks }));
   }
 }
 
