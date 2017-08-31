abeba7a31fd9c9c2a087f79b17f86d7fcfbe93d4
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-31 14:21:00 -0700

    adds placeholder graphs

--- src/components/visualizations.js
+++ src/components/visualizations.js
@@ -1,13 +1,16 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryPie, VictoryLabel } from 'victory';
-
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+import _ from 'lodash';
+import { VictoryPie,
+         VictoryLabel,
+         VictoryChart,
+         VictoryStack,
+         VictoryGroup,
+         VictoryArea,
+         VictoryPortal,
+         VictoryScatter,
+         VictoryTheme
+        } from 'victory';
 
 class Visualization extends React.Component {
 
@@ -15,13 +18,151 @@ class Visualization extends React.Component {
     super();
     this.state = {
       femaleArtistCount: 0,
-      maleArtistCount: 0
+      maleArtistCount: 0,
+      data: this.getData()
     };
   }
 
   componentWillMount() {
     this.getArtistCount("female", "femaleArtistCount");
     this.getArtistCount("male", "maleArtistCount");
+
+    fetch(`/api/collection/artworks/?date_display=1900&department=Photography`)
+      .then(res => res.json())
+      .catch(e => e)
+      .then(res => console.log(res.count));
+  }
+
+  componentDidMount() {
+    this.setStateInterval = window.setInterval(() => {
+      this.setState({ data: this.getData() });
+    }, 4000);
+  }
+
+  render() {
+    // Sets up gender breakdown graph
+    var artistGenderBreakdown =
+      <div>
+        <svg viewBox="0 0 400 400">
+          <VictoryPie
+            width={400} height={400}
+            data={[
+              { x: "27.38% Female", y: this.state.femaleArtistCount },
+              { x: "72.62% Male", y: this.state.maleArtistCount }
+            ]}
+            innerRadius={68} labelRadius={210}
+            style={{ labels: { fontSize: 20, fill: "black"}}}
+            colorScale={["tomato", "orange"]}
+            animate={{ duration: 2000 }}
+          />
+        </svg>
+      </div>
+
+    // Painting and Sculpture, Photography, Architecture and Design over time
+    var stackedMediumOverTime =
+      <div>
+        <VictoryChart scale={{x: "time"}} width={400} height={400}>
+          <VictoryStack colorScale="warm">
+            <VictoryGroup
+              data={[
+                {x: new Date(1986, 1, 1), y: 2},
+                {x: new Date(1996, 1, 1), y: 3},
+                {x: new Date(2006, 1, 1), y: 5},
+                {x: new Date(2016, 1, 1), y: 4}
+              ]}
+            >
+              <VictoryArea/>
+              <VictoryPortal>
+                <VictoryScatter
+                  style={{ data: {fill: "black"}}}
+                />
+              </VictoryPortal>
+            </VictoryGroup>
+            <VictoryGroup
+              data={[
+                {x: new Date(1986, 1, 1), y: 4},
+                {x: new Date(1996, 1, 1), y: 3},
+                {x: new Date(2006, 1, 1), y: 2},
+                {x: new Date(2016, 1, 1), y: 5}
+              ]}
+            >
+              <VictoryArea/>
+             <VictoryPortal>
+                <VictoryScatter
+                  style={{ data: {fill: "black"}}}
+                />
+              </VictoryPortal>
+            </VictoryGroup>
+            <VictoryGroup
+              data={[
+                {x: new Date(1986, 1, 1), y: 3},
+                {x: new Date(1996, 1, 1), y: 1},
+                {x: new Date(2006, 1, 1), y: 4},
+                {x: new Date(2016, 1, 1), y: 2}
+              ]}
+            >
+              <VictoryArea/>
+              <VictoryPortal>
+                <VictoryScatter
+                  style={{ data: {fill: "black"}}}
+                />
+              </VictoryPortal>
+            </VictoryGroup>
+          </VictoryStack>
+        </VictoryChart>
+      </div>
+
+    // Some data on this chart here
+    var chart =
+    <div>
+      <VictoryChart
+        theme={VictoryTheme.material}
+        animate={{ duration: 1000 }}
+      >
+        <VictoryStack
+          colorScale={"warm"}
+        >
+          {this.state.data.map((data, i) => {
+            return (
+              <VictoryArea
+                key={i}
+                data={data}
+                interpolation={"basis"}
+              />
+            );
+          })}
+        </VictoryStack>
+      </VictoryChart>
+    </div>
+
+    return (
+      <div className="row">
+        <div className="col-md-4 text-center">
+          <h1>Art Stats</h1>
+          { chart }
+        </div>
+        <div className="col-md-4 text-center">
+          <h1>Gender Breakdown</h1>
+          { artistGenderBreakdown }
+        </div>
+        <div className="col-md-4 text-center">
+          <h1>Art Stats</h1>
+          { stackedMediumOverTime }
+        </div>
+      </div>
+    );
+  }
+
+  getData() {
+    return _.range(7).map(() => {
+      return [
+        { x: 1, y: _.random(1, 5) },
+        { x: 2, y: _.random(1, 10) },
+        { x: 3, y: _.random(2, 10) },
+        { x: 4, y: _.random(2, 10) },
+        { x: 5, y: _.random(2, 15) }
+      ];
+    });
   }
 
   getArtistCount(gender, counter) {
@@ -29,35 +170,6 @@ class Visualization extends React.Component {
       .then(res => res.json())
       .catch(e => e)
       .then(res => this.setState({ [counter]: res.count }));
-  }
-
-  render() {
-    var artistGenderBreakdown =
-      <svg viewBox="0 0 400 400">
-        <VictoryPie
-          width={400} height={400}
-          data={[
-            { x: "Female", y: this.state.femaleArtistCount },
-            { x: "Male", y: this.state.maleArtistCount }
-          ]}
-          innerRadius={68} labelRadius={100}
-          style={{ labels: { fontSize: 20, fill: "white"}}}
-        />
-        <VictoryLabel
-          textAnchor="middle"
-          x={200} y={200}
-          text="Gender Breakdown"
-        />
-      </svg>
-
-    return (
-      <div className="row">
-        <div className="col-md-3 text-center">
-          <h1>Art Stats</h1>
-          { artistGenderBreakdown }
-        </div>
-      </div>
-    );
   }
 }
 
