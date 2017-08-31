b6408a8d17286c210bef310ec399051c7bcba541
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-31 10:51:18 -0700

    broke genderBreakdown into variable

--- src/components/visualizations.js
+++ src/components/visualizations.js
@@ -24,37 +24,40 @@ class Visualization extends React.Component {
     this.getArtistCount("male", "maleArtistCount");
   }
 
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-3 text-center">
-          <h1>Art Stats</h1>
-            <svg viewBox="0 0 400 400">
-              <VictoryPie
-                width={400} height={400}
-                data={[
-                  { x: "Female", y: this.state.femaleArtistCount },
-                  { x: "Male", y: this.state.maleArtistCount }
-                ]}
-                innerRadius={68} labelRadius={100}
-                style={{ labels: { fontSize: 20, fill: "white"}}}
-              />
-              <VictoryLabel
-                textAnchor="middle"
-                x={200} y={200}
-                text="Gender Breakdown"
-              />
-            </svg>
-        </div>
-      </div>
-    );
-  }
-
   getArtistCount(gender, counter) {
     fetch(`/api/collection/artists/?gender=${gender}`)
       .then(res => res.json())
       .catch(e => e)
       .then(res => this.setState({ [counter]: res.count }));
+  }
+
+  render() {
+    var artistGenderBreakdown =
+      <svg viewBox="0 0 400 400">
+        <VictoryPie
+          width={400} height={400}
+          data={[
+            { x: "Female", y: this.state.femaleArtistCount },
+            { x: "Male", y: this.state.maleArtistCount }
+          ]}
+          innerRadius={68} labelRadius={100}
+          style={{ labels: { fontSize: 20, fill: "white"}}}
+        />
+        <VictoryLabel
+          textAnchor="middle"
+          x={200} y={200}
+          text="Gender Breakdown"
+        />
+      </svg>
+
+    return (
+      <div className="row">
+        <div className="col-md-3 text-center">
+          <h1>Art Stats</h1>
+          { artistGenderBreakdown }
+        </div>
+      </div>
+    );
   }
 }
 
