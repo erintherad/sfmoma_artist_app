895313dcc9b764ce91ec7e6ad145cfa846c57d73
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-30 17:23:13 -0700

    adds Victory for visualizations

--- src/components/visualizations.js
+++ src/components/visualizations.js
@@ -1,5 +1,13 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
+import { VictoryBar } from 'victory';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
 
 class Visualization extends React.Component {
 
@@ -13,8 +21,15 @@ class Visualization extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="col-md-12 text-center">
+        <div className="col-md-3 text-center">
           <h1>Art Stats</h1>
+            <VictoryBar
+              data={data}
+              // data accessor for x values
+              x="quarter"
+              // data accessor for y values
+              y="earnings"
+            />
         </div>
       </div>
     );
