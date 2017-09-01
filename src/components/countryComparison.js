c384e273b1d0062ee007dffcdf57f0bac5a76eb5
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-01 12:33:30 -0700

    updates with correct data for comparison

--- src/components/countryComparison.js
+++ src/components/countryComparison.js
@@ -1,62 +1,57 @@
-import React    from 'react';
-import { VictoryBar,
-         VictoryAxis,
-         VictoryLabel,
-         VictoryStack
-        } from 'victory';
+import React from 'react';
+import { VictoryChart, VictoryStack, VictoryBar, VictoryAxis } from 'victory';
 
-const dataA = [
-  {x: "Photography", y: 57},
-  {x: "Painting", y: 40},
-  {x: "Architecture", y: 38},
-  {x: "video", y: 37}
+const myDataset = [
+  [
+      {x: "Photography", y: 4165},
+      {x: "Architecture & Design", y: 63},
+      {x: "Painting & Sculpture", y: 408},
+      {x: "Media Arts", y: 14}
+  ],
+  [
+      {x: "Photography", y: 638},
+      {x: "Architecture & Design", y: 844},
+      {x: "Painting & Sculpture", y: 66},
+      {x: "Media Arts", y: 0}
+  ]
 ];
 
-const dataB = dataA.map((point) => {
-  const y = Math.round(point.y + 3 * (Math.random() - 0.5));
-  return { ...point, y };
-});
-
-const width = 200;
-const height = 200;
-
 export default class CountryComparison extends React.Component {
+  transformData(dataset) {
+    const totals = dataset[0].map((data, i) => {
+      return dataset.reduce((memo, curr) => {
+        return memo + curr[i].y;
+      }, 0);
+    });
+    return dataset.map((data) => {
+      return data.map((datum, i) => {
+        return {x: datum.x, y: (datum.y / totals[i]) * 100};
+      });
+    });
+  }
 
   render() {
-    var countryComparison =
-    <svg viewBox={`0 0 200 200`}
-      style={{ width: "100%", height: "auto" }}
-    >
-      <VictoryStack horizontal
-        height={height}
-        width={width}
-        style={{ data: { width: 20 }, labels: { fontSize: 11 } }}
+    const dataset = this.transformData(myDataset);
+    const countryComparison =
+    <div>
+      <VictoryChart height={400} width={400}
+        domainPadding={{x: 30, y: 20}}
       >
-        <VictoryBar
-          style={{ data: { fill: "orange" } }}
-          data={dataA}
-          y={(data) => (-Math.abs(data.y))}
-          labels={(data) => (`${Math.abs(data.y)}%`)}
-        />
-        <VictoryBar
-          style={{ data: { fill: "gold" } }}
-          data={dataB}
-          labels={(data) => (`${Math.abs(data.y)}%`)}
-        />
-      </VictoryStack>
-
-      <VictoryAxis dependentAxis
-        height={height}
-        width={width}
-        style={{
-          axis: { stroke: "transparent" },
-          ticks: { stroke: "transparent" },
-          tickLabels: { fontSize: 8, fill: "black" }
-        }}
-        tickLabelComponent={<VictoryLabel x={100} textAnchor="middle"/>}
-        tickValues={dataA.map((point) => point.x).reverse()}
-      />
-    </svg>
+          <VictoryStack
+            colorScale={["tomato", "orange", "gold"]}
+          >
+            {dataset.map((data, i) => {
+              return <VictoryBar data={data} key={i}/>;
+            })}
+          </VictoryStack>
+          <VictoryAxis dependentAxis
+            tickFormat={(tick) => `${tick}%`}
+          />
+          <VictoryAxis
+            tickFormat={["Photography", "Architecture", "Painting", "Media Arts"]}
+          />
+      </VictoryChart>
+    </div>
 
     return (
       <div className="col-md-4">
