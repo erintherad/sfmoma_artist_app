711d93714b8dbedac5b9b58fe2e836c339979651
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-04 09:59:04 -0700

    adds result type

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -8,7 +8,8 @@ export default class Artwork extends React.Component {
     super();
     this.state = {
       artworks: [],
-      resultCount: 0
+      resultCount: 0,
+      type: ""
     };
   }
 
@@ -36,11 +37,11 @@ export default class Artwork extends React.Component {
         <div className="row white-container-style">
           <div className="col-md-12 artwork-input-container">
             <input className="form-control text-center search-input"
-                     placeholder="Search by sculpture, drawing, video, painting..."
+                     placeholder="Search by keyword: clay, paint, video..."
                      ref="query"
                      onChange={ (e) => { this.search(this.refs.query.value); } }
                      type="text" />
-             <div className="results-div">{ this.state.resultCount } results</div>
+                   <div className="results-div">{ this.state.resultCount } <span className="orange">{ this.state.type }</span> results</div>
           </div>
           <div className="col-md-12 artwork-results-container">
             <div className="card-deck">{ artworks }</div>
@@ -50,10 +51,10 @@ export default class Artwork extends React.Component {
     );
   }
 
-  search(type = "photograph") {
-    fetch(`/api/collection/artworks/?page_size=8&has_images=1&type=${type}`)
+  search(type = "photo") {
+    fetch(`/api/collection/artworks/?page_size=8&has_images=1&object_keywords__icontains=${type}`)
       .then(res => res.json())
       .catch(e => e)
-      .then(res => this.setState({ artworks: res.results, resultCount: res.count }));
+      .then(res => this.setState({ artworks: res.results, resultCount: res.count, type: `${type}` }));
   }
 }
